527970d3f44338366e521e37108ea5d4edf9e694
Zahar Fedorchuk <128075241+FZahaRV@users.noreply.github.com>
2023-09-01 20:50:01 +0300

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,7 @@
 public class Test {
         public static void main(String[] args) throws InterruptedException {
             MultithreadingTimer multithreading = new MultithreadingTimer();
-            multithreading.multithreadingTimer(10);
+            multithreading.multithreadingTimer(24);
             FizzbuzzCounter counter = new FizzbuzzCounter();
             counter.fizzbuzzCounter(99);
         }
